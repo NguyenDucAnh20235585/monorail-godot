88e59660731d62d766cc6ff37c3cebb32dbac17e
parkdayoung <kei08032019@gmail.com>
2026-08-17 10:44:29 +0700

    feat: add main menu and basic settings ui - anh.nd

---
+++ scenes/main_menu/main_menu.gd
@@ -0,0 +1,16 @@
+extends Control
+
+@onready var main_buttons: VBoxContainer = $MainButtons
+@onready var options: Label = $Options
+
+func _ready():
+	main_buttons.visible = true
+	options.visible = false
+
+func _on_settings_button_pressed():
+	main_buttons.visible = false
+	options.visible = true
+
+func _on_close_options_pressed():
+	main_buttons.visible = true
+	options.visible = false
